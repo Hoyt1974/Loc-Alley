0ac3f7897e6fd518d3fa73264052f036f273364a
Jason Hoyt <hoytjmatthews@gmail.com>
2021-03-12 06:33:00 -0500

    first commit

---
+++ pages/components/pics.js
@@ -0,0 +1,14 @@
+import Image from "next/image";
+
+function Picture() {
+  return (
+    <Image
+      src="/d842c4007ec410315ea1770a70718c51.jpg"
+      alt="pic of Arts and Crafts"
+      width={1000}
+      height={1000}
+    />
+  );
+}
+
+export default Picture;
